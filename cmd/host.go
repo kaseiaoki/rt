7ad7f24362a949fb4be3364b7c294368313a5390
aucuba <aoki.070207@gmail.com>
2021-01-12 15:07:31 +0900

    init2

---
+++ cmd/host.go
@@ -0,0 +1,62 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"errors"
+	"github.com/spf13/cobra"
+	"net/http"
+)
+var RedirectAttemptedError = errors.New("redirect")
+
+// hCmd represents the h command
+var hostCmd = &cobra.Command{
+	Use:   "host",
+	Short: "test with host name",
+	Long: `test with host name`,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return errors.New("Default argument is url(https://example.io)")
+		}
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		target_url := args[0]
+		req, _ := http.NewRequest("HEAD", target_url, nil)
+		resp, _ := http.DefaultTransport.RoundTrip(req)
+		defer resp.Body.Close()
+		if resp.StatusCode == 301 || resp.StatusCode == 302 || resp.StatusCode == 303 || resp.StatusCode == 307 {
+			fmt.Println(resp.Header["Location"])
+		}
+		return nil
+	},
+}
+
+
+func init() {
+	rootCmd.AddCommand(hostCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// hCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// hCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
