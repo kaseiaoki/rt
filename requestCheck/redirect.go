b4fd6cd52a6ec5fc55977c4b4ca4d37db030cb2a
aucuba <aoki.070207@gmail.com>
2021-01-12 17:50:42 +0900

    Update redirect.go

--- requestCheck/redirect.go
+++ requestCheck/redirect.go
@@ -2,23 +2,27 @@ package requestCheck
 
 import (
 	"net/http"
-	"reflect"
+    "reflect"
 )
 
-func AllRedirectHeader(target_url string) []string {
+func AllRedirectHeader(target_url string) ([]string, error){
 	req, _ := http.NewRequest("HEAD", target_url, nil)
-	resp, _ := http.DefaultClient.Do(req)
-    var a []string
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	var a []string
 	r := resp
-    for i := 0; r != nil; i++ {
-        rv := reflect.ValueOf(r).Elem()
-        vv := rv.FieldByName("Request")
-        rp, ok := vv.Interface().(*http.Request)
-        if !ok {
-            break
-        }
-        a = append(a, rp.URL.String())
-        r = rp.Response
-    }
-	return a
+	for i := 0; r != nil; i++ {
+		rv := reflect.ValueOf(r).Elem()
+		vv := rv.FieldByName("Request")
+		rp, ok := vv.Interface().(*http.Request)
+		if !ok {
+			break
+		}
+		a = append(a, rp.URL.String())
+		r = rp.Response
+	}
+	return a, err
 }
